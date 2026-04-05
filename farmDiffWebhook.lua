414ad251896edfcc59d304ccd0a9befe127c41bf
Atypical <142782288+Attypical@users.noreply.github.com>
2026-04-05 12:51:29 +0300

    Update farmDiffWebhook.lua

--- farmDiffWebhook.lua
+++ farmDiffWebhook.lua
@@ -23,19 +23,11 @@ end
 wait(14)
 
 task.spawn(function()
-    local allowancesClaimed = 0
-
-    _G.OnATMClaimed = function()
-        allowancesClaimed = allowancesClaimed + 1
-        _G.notify("> allowance " .. allowancesClaimed .. "/4 claimed!", 2)
-
-        if allowancesClaimed >= 4 then
-            _G.notify("> 4 allowances claimed, serverhopping...", 3)
-            wait(2)
-            pcall(function()
-                game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("RCTNMEUN"):InvokeServer()
-            end)
-        end
+    while true do
+        wait(50 * 60)
+        pcall(function()
+            game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("RCTNMEUN"):InvokeServer()
+        end)
     end
 end)
 
