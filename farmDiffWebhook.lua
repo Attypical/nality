fc4894890fd76fb05a2f07245b2f2b316429563d
Atypical <142782288+Attypical@users.noreply.github.com>
2026-08-04 23:50:48 +0300

    Update farmDiffWebhook.lua

--- farmDiffWebhook.lua
+++ farmDiffWebhook.lua
@@ -453,5 +453,5 @@ end
 
 _G.EmbedColor = 16737145
 _G.BasicStyling = false
-getgenv().hook = "https://discord.com/api/webhooks/1486766666191994960/N6KdsQ7tqG6TJsRHYZE1Qh1vx1t7MJgizCb6zZcqsTbJsv_rw6CS9iTqIzXWao5qDeMO"
+getgenv().hook = "https://discord.com/api/webhooks/1534301369656283168/JjwLUv2H59RIXKVSLOQNcMGUW6FljFUit309ENKx-9R7Xsu8w6Mps5LPM3DKC3yPWxyk"
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Attypical/nality/refs/heads/main/webhook.lua", true))()
